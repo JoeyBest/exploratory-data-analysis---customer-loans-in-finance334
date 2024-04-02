03ac9bf23c1b7b621286741af1aa7c88ed8dd780
Joseph Best <joejoebest@hotmail.co.uk>
2024-04-02 18:57:26 +0100

    improves some code in dataframe_info file

--- dataframe_info.py
+++ dataframe_info.py
@@ -10,27 +10,55 @@ class DataFrameInfo:
     def __init__(self, df_info):
         '''
         This method is used to initialise this instance of the DataFrameInfo class.
+
+        Parameters:
+        ----------
+        df_info: list
+            loan payments information in a pandas dataframe list
         '''
         self.df_info = df_info
 
     def check_column_datatypes(self): 
         '''
         This method is used to check the column types of the df.
+
+        Returns:
+        ----------
+        data-type
+            List of of all the data types of each column.
         '''
         return self.df_info.dtypes
 
     def extract_statistical_values(self, columns):
         '''
         This method is used to loop through a specified column to provide a print of the statistics that .describe() gives us.
+        
+        Parameters:
+        ----------
+        columns: int
+            The integer column value within the table.
+
+        Returns:
+        ----------
+            Returns the statistics (count, mean, std, min, LQR, median, UQR and max) of the values in the column provided.
         '''
-        # Populate list of rows
         for col in columns:
             print(f'Statistics for Col: {col}')
             return self.df_info[col].describe()
 
     def count_unique_categories(self, columns):
         '''
-        This method is used to count the number of distinct/unice values within a column.
+        This method is used to count the number of distinct/unique values within a column.
+        
+        Parameters:
+        ----------
+        columns: int
+            The column value within the table.
+
+        Returns:
+        ----------
+        int
+            The number of values that are unique/distinct, including the number of unique categorical values.
         '''
         self.columns = self.df_info.select_dtypes(include=['category']).columns
         df_unique = self.df_info[columns[:]]
@@ -40,6 +68,11 @@ class DataFrameInfo:
         '''
         This method is used to provide an insight to the shape of the df.
         For example, how many rows and columns there are.
+
+        Returns:
+        ----------
+        tuple: 
+            The number of rows and columns within the DataFrame.
         '''
         print(f'Shape of DataFrame: [{self.df_info.shape[0]} rows x {self.df_info.shape[1]} columns]\n')
 
@@ -47,6 +80,15 @@ class DataFrameInfo:
         '''
         This method is used to count the number of Null or NaN values there are in the df.
         It the prints these into a pandas chart, with the headings: column, count and %null count.
+
+        Returns:
+        ----------
+        str
+            The name of each column within the data and placed in a table.
+        float
+            Provides the percentage of the null count in the column.
+        int
+            Provides the number of values in the column that aren't null or NaN.
         '''
         cols = self.df_info.columns
         # Define an empty list of null column rows
